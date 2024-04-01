fbc84dcb1cbbd046e0196e0c3e2a5f68eb55c651
ramansaini14 <ramandeepsinghsaini55@gmail.com>
2024-04-02 03:40:41 +0530

    myCommit

---
+++ app/src/main/java/com/iitdh/sonusourav/instigo/TestActivity.java
@@ -0,0 +1,15 @@
+package com.iitdh.sonusourav.instigo;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TestActivity extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test);
+
+    }
+
+
+}
